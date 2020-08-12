beb84fce29c033f2f63c3efad37ea9bbca91ef25
Virginia Rosianu <gina.rosianu@gmail.com>
2020-08-12 14:09:04 +0300

    Add. Added initial tests

---
+++ src/main/java/pageObject/AuthenticationPage.java
@@ -0,0 +1,27 @@
+package pageObject;
+
+import org.junit.Assert;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class AuthenticationPage {
+
+    private WebDriver webDriver;
+
+    public AuthenticationPage(WebDriver driver) {
+        this.webDriver = driver;
+
+    }
+
+    @FindBy(xpath = "//*[@class='page-heading']")
+    private WebElement pageTitle;
+
+    public void checkPageTitle(String expectedTitle) {
+        Assert.assertTrue(expectedTitle.equals(pageTitle.getText()));
+    }
+
+//    public void enterEmailAddress(String emailAddres){
+//        pageTitle.sendKeys(emailAddres);
+//    }
+}
---
+++ src/test/java/storeTest/LoginTest.java
@@ -0,0 +1,22 @@
+package storeTest;
+
+import org.junit.Test;
+
+public class LoginTest extends BaseTest {
+    @Test
+    public void shouldLogin() {
+        homePage.clickSignInButton();
+        authenticationPage.checkPageTitle("AUTHENTICATION");
+//        authenticationPage.enterEmailAddress("validemai@asdasd.com");
+        //username
+        //password
+
+    }
+@Test
+
+    // Enter username
+    // Enter password
+    // Click login/signin
+    // Verify page title
+    // Verify login is made with correct email/username
+}
